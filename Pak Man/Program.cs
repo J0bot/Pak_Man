b83df0f260b65a915714206de70b95e4c012c903
Jobot <jojo64364@gmail.com>
2021-03-18 14:39:24 +0100

    Initial commit with Monogame (amod()

--- Pak Man/Program.cs
+++ Pak Man/Program.cs
@@ -2,11 +2,13 @@
 
 namespace Pak_Man
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using (var game = new Game1())
+                game.Run();
         }
     }
 }
